fda5dc72a5c322a9ccb318b9788a2768cddbc753
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-06-03 18:34:31 +0530

    socket.js

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,3 +1,4 @@
+import Message from '../models/message.model.js';
 import User from '../models/user.model.js'
 
 export const getAllUsers = async (req,res,next)=>{
@@ -12,4 +13,23 @@ export const getAllUsers = async (req,res,next)=>{
         console.log("error in getUser controller",error);
         next(error)
     }
+}
+
+export const getMessages = async(req,res,next)=>{
+  try {
+    const myId = req.auth.userId
+    const {userId} = req.params
+
+    const message = await Message.find({
+      $or:[
+        {senderId:userId,recieverId:myId},
+        {senderId:myId,recieverId:userId}
+      ]
+    }).sort({createdAt :1})
+
+    res.status(200).json(message)
+  } catch (error) {
+    console.log("error in getMessage controller",error);
+    next(error)
+  }
 }
