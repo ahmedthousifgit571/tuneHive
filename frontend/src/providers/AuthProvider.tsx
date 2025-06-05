fca17a9c7214640bb0356a5ba1ced9baf258d572
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-06-05 18:10:43 +0530

    client-socket config

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/lib/axios"
 import { useAuthStore } from "@/stores/useAuthStore"
+import { useChatStore } from "@/stores/useChatStore"
 import { useAuth } from "@clerk/clerk-react"
 import { Loader } from "lucide-react"
 import { useEffect, useState } from "react"
@@ -14,9 +15,10 @@ const updateApiToken = (token:string | null)=>{
 }
 
 const AuthProvider = ({children}:{children:React.ReactNode}) => {
-    const {getToken} = useAuth()
+    const {getToken,userId} = useAuth()
     const [loading,setLoading] = useState(true)
      const {checkAdmin} = useAuthStore()
+     const {initSocket,disconnectSocket} = useChatStore()
 
     useEffect(()=>{
     const initAuth = async()=>{
@@ -25,6 +27,10 @@ const AuthProvider = ({children}:{children:React.ReactNode}) => {
           updateApiToken(token)
           if(token){
             await checkAdmin()
+            // init socket
+            if(userId){
+              initSocket(userId)
+            }
           }
         }catch(error){
             updateApiToken(null)
@@ -34,7 +40,9 @@ const AuthProvider = ({children}:{children:React.ReactNode}) => {
         }
     }
     initAuth()
-    },[getToken])
+    // cleanup function for disconnect socket
+    return () =>  disconnectSocket()
+    },[getToken,userId,initSocket,disconnectSocket,checkAdmin])
 
   if(loading) return (
     <div className="h-screen w-full flex items-center justify-center">
--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -1,17 +1,45 @@
 import { axiosInstance } from '@/lib/axios'
+import type { Message, User } from '@/types'
 import {create} from 'zustand'
+import {io} from 'socket.io-client'
 
 interface ChatStore{
     users: any[]
-    fetchUsers : ()=>Promise<void>
     isLoading: boolean
     error : string | null
+
+    socket: any
+    isConnected:boolean
+    onlineUsers:Set<string>
+    userActivities:Map<string, string>
+    messages:Message[]
+    selectedUser:User |null
+
+    fetchUsers : ()=>Promise<void>
+    initSocket : (userId:string) => void
+    disconnectSocket : ()=> void
+    sendMessage : (recieverId:string,senderId:string,message:string) => void
+    fetchMessages: (userId:string) =>Promise<void>
+    setSelectedUser:(user:User | null)=>void
 }
 
-export const useChatStore = create<ChatStore>((set)=> ({
+const baseUrl = "http://localhost:5000"
+const socket = io(baseUrl,{
+    autoConnect:false,
+})
+
+export const useChatStore = create<ChatStore>((set,get)=> ({
      users : [],
      isLoading : false,
      error : null,
+     socket:null,
+     isConnected:false,
+     onlineUsers:new Set(),
+     userActivities : new Map(),
+     messages: [],
+     selectedUser : null,
+     
+
 
     fetchUsers : async()=>{
         try {
@@ -22,5 +50,98 @@ export const useChatStore = create<ChatStore>((set)=> ({
         }finally{
             set({isLoading:false})
         }
+    },
+    
+    initSocket : async(userId:string)=>{
+      if(!get().socket.isConnected){
+        socket.auth = {userId}
+        socket.connect()
+        socket.emit("user_connected",userId)   //send an event
+
+        socket.on("user_online",(users:string[])=>{  //listend and set to onlineusers
+            set({onlineUsers:new Set(users)})
+        })
+ 
+        // listening to activites
+        socket.on("activities",(activities:[string,string][]) =>{   
+            set({userActivities: new Map(activities)})
+        })
+
+        // listening for connected users
+        socket.on("user_connected",(userId:string)=>{
+            set((state)=> ({
+                onlineUsers: new Set([...state.onlineUsers,userId])
+            }))
+        })
+
+        // listening for disconnected users
+        socket.on("user_disconnected",(userId:string)=>{
+            set((state)=> {
+                const newOnlineUsers = new Set(state.onlineUsers)
+                newOnlineUsers.delete(userId)
+                return {onlineUsers: newOnlineUsers}
+            })
+        })
+
+        // listening on recieved message:
+        socket.on("recieved_message",(message:Message)=>{
+            set((state) => ({
+                messages:[...state.messages,message]
+            }))
+        })
+
+        // listening on sent message
+        socket.on("message_sent",(message:Message)=>{
+            set((state) => ({
+                messages:[...state.messages,message]
+            }))
+        })
+
+        // listening on updated activity
+        socket.on("activity_updated",({userId,activity})=>{
+            set((state)=> {
+                    const newActivities = new Map(state.userActivities)
+                    newActivities.set(userId,activity)
+                    return {userActivities:newActivities}
+            })
+        })
+
+        set({isConnected:true})
+      }
+    },
+    
+    disconnectSocket: async()=>{
+        if(get().isConnected){
+        socket.disconnect()
+        set({isConnected:false})
+        }
+        
+    },
+
+    sendMessage : async(recieverId,senderId,content)=>{
+      const socket = get().socket
+      if(!socket) return 
+
+     // sending message
+     socket.emit("send_message",{recieverId,senderId,content})  
+    },
+
+    fetchMessages: async(userId) => {
+        set({isLoading:true,error:null})
+        try {
+           const response =  await axiosInstance.get(`/users/messages/${userId}`)
+           set({messages:response.data})
+        } catch (error:any) {
+            console.log("error in fetch messages:",error)
+            set({error:error.response.data.message})
+        }finally{
+            set({isLoading:false})
+        }
+    },
+
+    setSelectedUser : (user) =>{
+       set({selectedUser: user})
     }
+
+
 }))
