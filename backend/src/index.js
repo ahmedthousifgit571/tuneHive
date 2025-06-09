b58e1a8c504c33dcc153839f1c98a6b4214970d6
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-06-09 18:15:42 +0530

    production changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,10 @@ import { createServer } from 'http'
 import fileUpload from 'express-fileupload'
 import path from 'path'
 import cors from 'cors'
+import fs from "fs"
 import { fileURLToPath } from 'url'
 import { initializeSocket } from './lib/socket.js'
+import cron from "node-cron"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -47,12 +49,37 @@ app.use(fileUpload({
 }))      
 
 
+const tempDir = path.join(process.cwd(),"tmp")
+// cron jobs
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
+
+
 app.use("/api/users",userRoutes)
 app.use("/api/auth",authRoutes)
 app.use("/api/admin", adminRoutes)
 app.use("/api/songs",songRoutes)
 app.use("/api/albums",albumRoutes)
 app.use("/api/stats",statsRoutes)
+
+// for production ready
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"))
+  })
+}
 
 
 // error handler
