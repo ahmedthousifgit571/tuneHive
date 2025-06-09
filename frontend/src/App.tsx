7841f7309ffdec43fe67dd492ad3c09b748cbe3a
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-06-09 17:42:06 +0530

    not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import ChatPage from "./pages/chat-page/ChatPage"
 import AlbumPage from "./pages/album-page/AlbumPage"
 import AdminPage from "./pages/admin-page/AdminPage"
 import {Toaster} from 'react-hot-toast'
+import NotFoundPage from "./pages/404/NotFoundPage"
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={ <HomePage />}/>
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/albums/:albumId" element={<AlbumPage/>}/>
+        <Route path="*" element={<NotFoundPage />}/>
        </Route>
 
      </Routes>
